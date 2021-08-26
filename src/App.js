5e0130cc96c48c1985d23b2f09c01c39b20fce1d
SiddiquiMonis401 <monisms15@gmail.com>
2021-08-26 18:31:29 +0500

    configure eslint

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img alt="logo" className="App-logo" src={logo} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" rel="noopener noreferrer" target="_blank">
           Learn React
         </a>
       </header>
