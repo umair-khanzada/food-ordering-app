debbd78ad50bc1cd14e33dff568091893432ddb7
SMY <smyaseen164@gmail.com>
2021-08-29 16:08:36 +0500

    Added Store Config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+import './index.css';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
