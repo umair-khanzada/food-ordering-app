01719ba6b3189fe82bfff26064ac18f45ec23248
SMY <smyaseen164@gmail.com>
2021-08-29 16:08:15 +0500

    A sample scenario created for login logout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import React from 'react';
+import { Actions } from './redux/actions';
 
-function App() {
+const App = ({ isLoggedIn, loginEpic }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>is Logged In: {isLoggedIn.toString()}</h1>
+      <button onClick={loginEpic} type="button">
+        Log In
+      </button>
     </div>
   );
-}
+};
 
-export default App;
+const mapStateToProps = (state) => ({
+  isLoggedIn: state.isLoggedIn,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  loginEpic: () => dispatch(Actions.login()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
