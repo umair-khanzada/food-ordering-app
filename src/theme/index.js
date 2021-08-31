4f9a793aa2899207bd52a5db32466dffefb9ca7d
SMY <smyaseen164@gmail.com>
2021-08-31 14:48:57 +0500

    added shapes and took out typography to its separte js file

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,18 +1,17 @@
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 import { MaterialPalleteColours } from './Colors';
-
+import { typography } from './Typography';
+// eslint-disable-next-line import/order
+import React from 'react';
+import customShapes from './shapes';
 
 const theme = createTheme({
-    palette: MaterialPalleteColours,
-    typography :{
-        fontFamily: ["Lato-Regular","Lato-Black" , "Lato-BlackItalic" , "Lato-Bold" , "Lato-BoldItalic" , "Lato-Italic" , "Lato-Light" , "Lato-LightItalic" , "Lato-Thin" , "Lato-ThinItalic"].join(","),
-    }
-
+  palette: MaterialPalleteColours,
+  typography,
+  shape: customShapes,
 });
 
-export default function MaterialThemeWrapper({children}) {
-    // we can use the light dark theme modal. If we need
-    return <ThemeProvider theme={theme}>
-        {children}
-    </ThemeProvider>
+export default function MaterialThemeWrapper({ children }) {
+  // we can use the light dark theme modal. If we need
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
