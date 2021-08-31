201e2c0d1fb6841d697845a5d9b70e693534d7f8
SiddiquiMonis401 <monisms15@gmail.com>
2021-08-31 14:52:30 +0500

    add container

--- src/Features/Login/route.js
+++ src/Features/Login/route.js
@@ -1,7 +1,9 @@
 import RouteNames from '../../routes/RouteNames';
 import { isPublicRoute } from '../../routes/Permission';
+import LoginContainer from './LoginContainer';
 
 export const loginRoute = {
     path: RouteNames.login,
     permissions: isPublicRoute, // to intimate it is public route
-} 
+    component: LoginContainer,
+}; 
