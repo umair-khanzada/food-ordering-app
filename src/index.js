b38e1fdfab2c802955d9bad69dca1247f9b2c15a
SMY <smyaseen164@gmail.com>
2021-08-31 14:47:29 +0500

    Added CssBaseline and GlobalStyles

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
+import { CssBaseline } from '@material-ui/core';
+import React from 'react';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './theme/GlobalStyles';
 import MaterialThemeWrapper from './theme';
+// eslint-disable-next-line import/order
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <MaterialThemeWrapper>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <CssBaseline />
+    <GlobalStyles />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </MaterialThemeWrapper>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
