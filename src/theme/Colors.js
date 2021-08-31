5af65b4b24875075e7d8f89a3da0d6a2efa11bf7
SMY <smyaseen164@gmail.com>
2021-08-31 14:48:04 +0500

    Added default background

--- src/theme/Colors.js
+++ src/theme/Colors.js
@@ -1,34 +1,42 @@
-import { green, blue, red, yellow, purple} from '@material-ui/core/colors';
+import { blue, green, grey, purple, red, yellow } from '@material-ui/core/colors';
 
 export const MaterialPalleteColours = {
-    primary: {
-        light: green[300],
-        main: green[500],
-        dark: green[900]
-    },
-    secondary: {
-        light: blue[300],
-        main: blue[500],
-        dark: blue[900]
-    },
-    error: {
-        light: red[300],
-        main: red[500],
-        dark: red[900]
-    },
-    warning: {
-        light: yellow[300],
-        main: yellow[500],
-        dark: yellow[900]
-    },
-    info: {
-        light: purple[500],
-        main: purple[300],
-        dark: purple[900]
-    },
-    success: {
-        light: green[100],
-        main: green[200],
-        dark: green[300]
-    }
+  primary: {
+    light: green[300],
+    main: green[500],
+    dark: green[900],
+    contrastText: '#ffffff',
+  },
+  secondary: {
+    light: blue[500],
+    main: blue[700],
+    dark: blue[900],
+    contrastText: '#ffffff',
+  },
+  error: {
+    light: red[300],
+    main: red[500],
+    dark: red[900],
+  },
+  warning: {
+    light: yellow[300],
+    main: yellow[500],
+    dark: yellow[900],
+  },
+  info: {
+    light: purple[500],
+    main: purple[300],
+    dark: purple[900],
+  },
+  success: {
+    light: green[100],
+    main: green[200],
+    dark: green[300],
+  },
+  background: {
+    default: '#8FB9E3',
+  },
+  action: {
+    hover: grey[200],
+  },
 };
