b1ffb3b3a3ad4d9c6151adc00c9e99963700975c
SMY <smyaseen164@gmail.com>
2021-09-06 19:14:43 +0500

    added signup

--- src/Features/Auth/actions.js
+++ src/Features/Auth/actions.js
@@ -1,4 +1,4 @@
-import { LOGIN, LOGIN_SUCCESS, LOGOUT, LOGIN_ERROR } from '../../scripts/constants';
+import { LOGIN, LOGIN_SUCCESS, LOGOUT, LOGIN_ERROR, SIGNUP } from '../../scripts/constants';
 
 export const login = (data) => ({ type: LOGIN, payload: data });
 
@@ -6,3 +6,5 @@ export const logout = () => ({ type: LOGOUT });
 
 export const loginSuccess = () => ({ type: LOGIN_SUCCESS });
 export const loginError = () => ({ type: LOGIN_ERROR });
+
+export const signup = (data) => ({ type: SIGNUP, payload: data });
