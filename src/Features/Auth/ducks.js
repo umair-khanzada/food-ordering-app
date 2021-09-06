adc245ffc99db843b5fd0ea0a830fbf82f885928
SMY <smyaseen164@gmail.com>
2021-09-06 19:17:04 +0500

    removed excess reducer

--- src/Features/Auth/ducks.js
+++ src/Features/Auth/ducks.js
@@ -1,12 +1,4 @@
-import {
-  LOGIN,
-  LOGIN_SUCCESS,
-  LOGOUT,
-  LOGIN_ERROR,
-  SIGNUP,
-  SIGNUP_ERROR,
-  SIGNUP_SUCCESS,
-} from '../../scripts/constants';
+import { LOGIN, LOGIN_SUCCESS, LOGOUT, LOGIN_ERROR, SIGNUP } from '../../scripts/constants';
 
 export const loginReducer = (state = { isLoggedIn: false }, action) => {
   switch (action.type) {
@@ -22,20 +14,8 @@ export const loginReducer = (state = { isLoggedIn: false }, action) => {
     case LOGIN_ERROR:
       return { isLoggedIn: false };
 
-    default:
-      return state;
-  }
-};
-
-export const SignUpReducer = (state = { isLoggedIn: false }, action) => {
-  const { type } = action;
-  switch (type) {
     case SIGNUP: // must Proper const here
       return {};
-    case SIGNUP_SUCCESS:
-      return { isLoggedIn: true };
-    case SIGNUP_ERROR:
-      return { isLoggedIn: false };
 
     default:
       return state;
