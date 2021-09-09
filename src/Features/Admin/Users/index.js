fb31544b4e56761a5027f1d4e3a0f1ff0b2f6b52
Haris-moin <harismoin48@gmail.com>
2021-09-10 00:19:49 +0500

    store Vendor list data in mock folder from Vendor component

--- src/Features/Admin/Users/index.js
+++ src/Features/Admin/Users/index.js
@@ -5,53 +5,9 @@ import { Edit } from '@material-ui/icons';
 
 import CommonButton from '../../../components/Button/Button';
 import CustomTable from '../../../components/CustomTable';
+import { userList } from '../../../Mock/UserList';
 import { VendorTitleContainer, VendorTitle, DeleteIcon } from './style';
 function UsersList() {
-  const tableData = [
-    {
-      id: 1,
-      name: 'asad',
-      email: 'asad@gmail.com',
-      contact: '13131232',
-    },
-    {
-      id: 2,
-      name: 'kashan',
-      email: 'kashan@gmail.com',
-      contact: '13131232',
-    },
-    {
-      id: 3,
-      name: 'Baber',
-      email: 'Baber@gamil.com',
-      contact: '1309232',
-    },
-    {
-      id: 4,
-      name: 'sami',
-      email: 'sami@gmail.com',
-      contact: '23131232',
-    },
-    {
-      id: 5,
-      name: 'nasir',
-      email: 'nasir@gmail.com',
-      contact: '10901232',
-    },
-    {
-      id: 6,
-      name: 'Kashif',
-      email: 'kashif@gamil.com',
-      contact: '13131232',
-    },
-    {
-      id: 7,
-      name: 'yawar',
-      email: 'yawar@gmail.com',
-      contact: '13131232',
-    },
-  ];
-
   const editDelete = (
     <>
       <IconButton>
@@ -70,7 +26,7 @@ function UsersList() {
         <CommonButton property="Add Users" />
       </VendorTitleContainer>
 
-      <CustomTable editDelete={editDelete} header={header} rows={tableData} />
+      <CustomTable editDelete={editDelete} header={header} rows={userList} />
     </>
   );
 }
--- src/Features/Admin/VendorList/index.js
+++ src/Features/Admin/VendorList/index.js
@@ -5,66 +5,9 @@ import { Edit } from '@material-ui/icons';
 
 import CommonButton from '../../../components/Button/Button';
 import CustomTable from '../../../components/CustomTable';
+import { vendorList } from '../../../Mock/VendorList';
 import { VendorTitleContainer, VendorTitle, DeleteIcon } from './style';
 function VendorList() {
-  const tableData = [
-    {
-      id: 1,
-      name: 'Adnan',
-      email: 'adnan@gmail.com',
-      contact: '13131232',
-      timing: '1:40',
-      building: 'main',
-    },
-    {
-      id: 2,
-      name: 'Yousuf',
-      email: 'Yousuf@gmail.com',
-      contact: '13131232',
-      timing: '1:40',
-      building: 'main',
-    },
-    {
-      id: 3,
-      name: 'Baber',
-      email: 'Baber@gamil.com',
-      contact: '1309232',
-      timing: '1:00',
-      building: 'qasre sheeren',
-    },
-    {
-      id: 4,
-      name: 'Dilawer',
-      email: 'Dilawer@gmail.com',
-      contact: '23131232',
-      timing: '1:50',
-      building: 'qasre sheeren',
-    },
-    {
-      id: 5,
-      name: 'Naem',
-      email: 'Naem@gmail.com',
-      contact: '10901232',
-      timing: '1:10',
-      building: 'main',
-    },
-    {
-      id: 6,
-      name: 'Kashif',
-      email: 'kashif@gamil.com',
-      contact: '13131232',
-      timing: '1:40',
-      building: 'main',
-    },
-    {
-      id: 7,
-      name: 'Dilawer',
-      email: 'Dilawer@gmail.com',
-      timing: '1:10',
-      building: 'main',
-    },
-  ];
-
   const editDelete = (
     <>
       <IconButton>
@@ -84,7 +27,7 @@ function VendorList() {
         <CommonButton property="Add Vendor" />
       </VendorTitleContainer>
 
-      <CustomTable editDelete={editDelete} header={header} rows={tableData} />
+      <CustomTable editDelete={editDelete} header={header} rows={vendorList} />
     </>
   );
 }
