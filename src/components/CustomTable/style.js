22d954223730189b94bb25df397d26f9792174ff
Haris-moin <harismoin48@gmail.com>
2021-09-10 21:17:52 +0500

    update changes in eslint for crlf to lf

---
+++ src/components/CustomTable/style.js
@@ -0,0 +1,16 @@
+import { TableHead, TableCell, TableContainer } from '@material-ui/core';
+import styled from 'styled-components';
+
+export const CustomTableHead = styled(TableHead)`
+  background-color: #e91e63;
+`;
+export const IconContainer = styled.div`
+  flex-shrink: 0;
+`;
+export const EditDeleteCell = styled(TableCell)`
+  display: flex;
+`;
+export const CustomTableContainer = styled(TableContainer)`
+  width: ${({ tablewidth }) => tablewidth};
+  margin: auto;
+`;
