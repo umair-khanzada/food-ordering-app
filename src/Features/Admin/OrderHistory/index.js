f13050facb83bc6e5597683b1e92294459f80333
Haris-moin <harismoin48@gmail.com>
2021-09-12 13:02:58 +0500

    move mock data into Mock folder from component js file

--- src/Features/Admin/OrderHistory/index.js
+++ src/Features/Admin/OrderHistory/index.js
@@ -4,49 +4,10 @@ import IconButton from '@material-ui/core/IconButton';
 import { Edit } from '@material-ui/icons';
 
 import CustomTable from '../../../components/CustomTable';
+import { OrderHistoryList } from '../../../Mock/OrderHistory';
 import { VendorTitleContainer, VendorTitle, DeleteIcon } from './style';
 
 function OrderHistory() {
-  const tableData = [
-    {
-      id: 1,
-      name: 'Adnan',
-      contact: '13131232',
-    },
-    {
-      id: 2,
-      name: 'Yousuf',
-
-      contact: '13131232',
-    },
-    {
-      id: 3,
-      name: 'Baber',
-      contact: '1309232',
-    },
-    {
-      id: 4,
-      name: 'Dilawer',
-      contact: '23131232',
-    },
-    {
-      id: 5,
-      name: 'Naem',
-
-      contact: '10901232',
-    },
-    {
-      id: 6,
-      name: 'Kashif',
-      contact: '13131232',
-    },
-    {
-      id: 7,
-      name: 'Dilawer',
-      contact: '13131232',
-    },
-  ];
-
   const editDelete = (
     <>
       <IconButton>
@@ -65,7 +26,7 @@ function OrderHistory() {
         <VendorTitle>Orders</VendorTitle>
       </VendorTitleContainer>
 
-      <CustomTable editDelete={editDelete} header={header} rows={tableData} />
+      <CustomTable editDelete={editDelete} header={header} rows={OrderHistoryList} />
     </>
   );
 }
