fde612b5f2aa44ff8bcf882524e79123a72653b8
Fahad96-bit <fahadqureshy23@gmail.com>
2021-09-13 15:20:48 +0500

    categorylist-updated

---
+++ src/Features/Vendor/CategoryList/CategoryList.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import IconButton from '@material-ui/core/IconButton';
+import { Edit } from '@material-ui/icons';
+import { useHistory } from 'react-router';
+
+import CommonButton from '../../../components/Button/Button';
+import CustomTable from '../../../components/CustomTable';
+import { CategoriesTitleContainer, DeleteIcon } from './Style';
+
+export const categoryList = [
+  {
+    id: 1,
+
+    category: 'greavy',
+  },
+
+  {
+    id: 2,
+
+    category: 'bread',
+  },
+
+  {
+    id: 3,
+
+    category: 'rice',
+  },
+
+  {
+    id: 4,
+
+    category: 'roll',
+  },
+
+  {
+    id: 5,
+
+    category: 'burger',
+  },
+];
+
+function CategoryList() {
+  const History = useHistory();
+  const editDelete = (
+    <>
+      <IconButton>
+        <Edit />
+      </IconButton>
+
+      <IconButton>
+        <DeleteIcon />
+      </IconButton>
+    </>
+  );
+
+  const header = ['Id', 'Category'];
+
+  return (
+    <>
+      <CategoriesTitleContainer>
+        <CommonButton onClick={() => History.push('/addcategory')} property="Add Category" />
+      </CategoriesTitleContainer>
+
+      <CustomTable header={header} rows={categoryList} tablewidth="50%" />
+    </>
+  );
+}
+
+export default CategoryList;
