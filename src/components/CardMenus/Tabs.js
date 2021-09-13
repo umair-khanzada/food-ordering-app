772aeaec7f16820fd0bb2601493717670ce38a6c
areebali99 <areebali83@example.com>
2021-09-13 16:37:35 +0500

    complete add to cart journey

--- src/components/CardMenus/Tabs.js
+++ src/components/CardMenus/Tabs.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 
 import { Paper, Tab, Tabs } from '@material-ui/core';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import { useParams } from 'react-router-dom';
 
 import CardMenus from './CardMenus';
 
 const MainTab = (props) => {
-  const [value, setValue] = useState(2);
+  const [value, setValue] = useState();
+  const params = useParams();
+  console.log('params', params);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
