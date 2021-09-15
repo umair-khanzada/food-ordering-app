a173980df4714209a289c3e01fe57ca18f56f2f8
Fahad96-bit <fahadqureshy23@gmail.com>
2021-09-15 17:21:20 +0500

    changed colour and layout of the table

--- src/components/CustomTable/Style.js
+++ src/components/CustomTable/Style.js
@@ -1,12 +1,25 @@
 import { TableCell, TableContainer, TableHead } from '@material-ui/core';
 import styled from 'styled-components';
-export const CustomTableHead = styled(TableHead)`
-  background-color: #e91e63;
-`;
+
 export const EditDeleteCell = styled(TableCell)`
   display: flex;
 `;
+
+export const CustomTableHead = styled(TableHead)`
+  color: white;
+  background-color: #00b3e3;
+  & .MuiTableCell-head {
+    font-weight: bold;
+  }
+`;
+export const IconContainer = styled.div`
+  flex-shrink: 0;
+`;
 export const CustomTableContainer = styled(TableContainer)`
   width: ${({ tablewidth }) => tablewidth};
+  & .MuiTableCell-root {
+    padding: 5px 14px;
+    line-height: 3.4;
+  }
   margin: auto;
 `;
