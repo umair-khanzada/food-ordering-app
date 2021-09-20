531320ecc3a5c864506f877ae2e42b34cf203219
smyaseen <smyaseen164@gmail.com>
2021-09-20 12:02:48 +0500

    added shallow equal to useSelector

--- src/routes/Layout/MainContainer.js
+++ src/routes/Layout/MainContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Grid } from '@material-ui/core';
-import { useSelector } from 'react-redux';
+import { useSelector, shallowEqual } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import AppBar from '../../components/AppBar/AppBar';
@@ -15,7 +15,7 @@ function MainContainer() {
     return {
       isLoggedIn,
     };
-  });
+  }, shallowEqual);
 
   const baseRouter = <BaseRouter />;
 
