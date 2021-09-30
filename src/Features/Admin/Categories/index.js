99efba0ee36e7c4fb5df9de3f751d12fdea94bd2
Fahad96-bit <fahadqureshy23@gmail.com>
2021-09-30 16:50:12 +0500

    implemented isFetching

--- src/Features/Admin/Categories/index.js
+++ src/Features/Admin/Categories/index.js
@@ -1,20 +1,19 @@
 import React, { useEffect, useState } from 'react';
 
 import { useMutation } from 'react-query';
-import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router';
 
 import CommonButton from '../../../components/Button/Button';
 import CustomTable from '../../../components/CustomTable';
 import RouteNames from '../../../routes/RouteNames';
-import { AuthToken, GetHeader } from '../../../scripts/constants';
+import { GetHeader } from '../../../scripts/constants';
 import { deleteCategory } from './mutation';
 import { FetchCategories } from './request';
 import { CategoriesTitleContainer, CategoriesTitle } from './style';
 
 function CategoryList() {
   const { headers } = GetHeader();
-  const token = AuthToken();
+
   const { addCategory, editCategory } = RouteNames;
   const history = useHistory();
   const onEdit = ({ id }) => {
@@ -23,17 +22,14 @@ function CategoryList() {
       search: `?id=${id}`,
     });
   };
-  const [categories, setCategories] = useState([]);
-  const { data: categoriesdata, refetch } = FetchCategories();
+  const { data: categoriesdata, refetch, isLoading, isFetching } = FetchCategories();
 
-  useEffect(() => {
-    if (Array.isArray(categoriesdata)) {
-      saveCategories(categoriesdata);
-    }
-  }, [categoriesdata]);
-  const saveCategories = (results) => {
-    setCategories(results);
-  };
+  // useEffect(() => {
+  //   if (Array.isArray(categoriesdata)) {
+  //     saveCategories(categoriesdata);
+  //   }
+  // }, [categoriesdata]);
+
   function deletecategory(categoryId) {
     console.log('categoryId', categoryId);
     mutate({ categoryId, headers });
@@ -44,13 +40,12 @@ function CategoryList() {
   };
 
   const [header, setHeader] = useState([]);
-  const dispatch = useDispatch();
 
   useEffect(() => {
     setHeader(['S.No', 'Categories', 'Edit']);
   }, []);
 
-  const { mutate, mutateAsync, isLoading, error } = useMutation(deleteCategory, {
+  const { mutate, mutateAsync, error } = useMutation(deleteCategory, {
     onSuccess: (response) => {
       refetch();
 
@@ -64,16 +59,22 @@ function CategoryList() {
         <CommonButton onClick={() => history.push(addCategory)} property="Add Category" />
       </CategoriesTitleContainer>
 
-      <CustomTable
-        cellWidth="400px"
-        deleteTableRow={deletecategory}
-        header={header}
-        isEditDelete
-        onDelete={onDelete}
-        onEdit={onEdit}
-        rows={categories}
-        tablewidth="90%"
-      />
+      {isFetching ? (
+        <>{console.log('loader will be here in another branch')}</>
+      ) : (
+        <>
+          <CustomTable
+            cellWidth="400px"
+            deleteTableRow={deletecategory}
+            header={header}
+            isEditDelete
+            onDelete={onDelete}
+            onEdit={onEdit}
+            rows={categoriesdata}
+            tablewidth="90%"
+          />
+        </>
+      )}
     </>
   );
 }
