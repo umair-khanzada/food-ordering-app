d04d54870da5f703928b01f9adac402d7cece956
smyaseen <smyaseen164@gmail.com>
2021-10-01 19:05:05 +0500

    refactored buttons array

--- src/Features/Admin/Categories/AddCategory/index.js
+++ src/Features/Admin/Categories/AddCategory/index.js
@@ -57,16 +57,14 @@ const AddCategory = () => {
     },
   });
 
-  const buttons = {
-    button: [
-      {
-        type: 'button',
-        name: 'save',
-        minWidth: '100%',
-        clickHandler: saveHandler,
-      },
-    ],
-  };
+  const buttons = [
+    {
+      type: 'button',
+      name: 'save',
+      minWidth: '100%',
+      clickHandler: saveHandler,
+    },
+  ];
   return (
     <CommonGridBasedForm
       buttons={buttons}
