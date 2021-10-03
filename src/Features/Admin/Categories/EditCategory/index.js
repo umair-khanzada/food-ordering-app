73cc5e585701771d86afa2d0de4799e86a157d60
Fahad96-bit <fahadqureshy23@gmail.com>
2021-10-03 19:46:06 +0500

    pulled edit Apis branch

--- src/Features/Admin/Categories/EditCategory/index.js
+++ src/Features/Admin/Categories/EditCategory/index.js
@@ -1,11 +1,45 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
+
+import { useMutation } from 'react-query';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 
 import CommonGridBasedForm from '../../../../components/CommonGridBasedForm';
 import { TEXT_FIELD } from '../../../../components/CommonGridBasedForm/FieldTypes';
+import { GetHeader } from '../../../../scripts/constants';
 import { validateOnSubmit, fieldChangeHandler } from '../../../../util/CommonGridBasedFormUtils';
+import { updateCategoryById } from '../mutation';
+import { FetchCategoriesById } from '../request';
 
 const EditCategory = () => {
+  const { headers } = GetHeader();
+  const adminId = useSelector((state) => {
+    const {
+      authReducer: { id },
+    } = state;
+    return id;
+  });
+
+  const history = useHistory();
+  const params = new URLSearchParams(history.location.search);
+  const id = params.get('id');
+  const { data: categoriesId, refetch } = FetchCategoriesById(id);
+  const [categories, setCategoriesId] = useState([]);
+
+  useEffect(() => {
+    if (categoriesId !== undefined) {
+      saveCategoriesId(categoriesId);
+    }
+  }, [categoriesId]);
   const [onSaveSuccess, setOnSaveSuccess] = useState(false);
+  const saveCategoriesId = (categoriesId) => {
+    const { name } = categoriesId;
+
+    fields[0].value = name;
+    setFields(fields);
+    setCategoriesId(name);
+  };
   const [fields, setFields] = useState([
     {
       type: TEXT_FIELD,
@@ -24,17 +58,36 @@ const EditCategory = () => {
   const saveHandler = () => {
     const { validateArray, isValid } = validateOnSubmit(fields);
     setFields(validateArray);
-    isValid ? setOnSaveSuccess(true) : setOnSaveSuccess(false);
+
+    if (isValid) {
+      const name = fields.map(({ value }, index) => value);
+      mutate({
+        category: {
+          name: name[0],
+          createdBy: adminId,
+        },
+        headers,
+        categoriesId,
+      });
+    }
   };
 
-  const buttons = [
-    {
-      type: 'button',
-      name: 'save',
-      minWidth: '100%',
-      clickHandler: saveHandler,
+  const buttons = {
+    button: [
+      {
+        type: 'button',
+        name: 'save',
+        minWidth: '100%',
+        clickHandler: saveHandler,
+      },
+    ],
+  };
+
+  const { mutate, mutateAsync, isLoading, error } = useMutation(updateCategoryById, {
+    onSuccess: (response) => {
+      return response;
     },
-  ];
+  });
 
   return (
     <CommonGridBasedForm buttons={buttons} fields={fields} heading="Edit Category" onSaveSuccess={onSaveSuccess} />
--- src/Features/Admin/Categories/request.js
+++ src/Features/Admin/Categories/request.js
@@ -19,3 +19,15 @@ export const FetchCategories = () => {
 
   return useQuery('categories', () => Categories(headers));
 };
+const CategoriesById = async (headers, id) => {
+  const { data } = await axios.get(baseUrl + 'categories/' + id, {
+    headers,
+  });
+  console.log('data', data);
+  return data;
+};
+export const FetchCategoriesById = (id) => {
+  const { headers } = GetHeader();
+
+  return useQuery('categories', () => CategoriesById(headers, id));
+};
