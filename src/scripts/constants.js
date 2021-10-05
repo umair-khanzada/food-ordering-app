d94cd1dc7afcff6a6284b711f859acbbdd1339b8
smyaseen <smyaseen164@gmail.com>
2021-10-05 16:05:59 +0500

    fixed email regex

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 
 import RouteNames from '../routes/RouteNames';
-export const emailRegex = new RegExp('^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$');
+export const emailRegex = new RegExp('\\S+@\\S+\\.\\S+');
 export const passwordRegex = new RegExp('^(.*(([A-Za-z]+(.*)[0-9]+)|([0-9]+(.*)[A-Za-z]+))(.*))$');
 export const contactRegex = new RegExp('^[0-9]{11,12}$');
 export const baseUrl = 'http://localhost:4000/v1/';
