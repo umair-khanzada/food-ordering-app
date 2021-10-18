bb17c98aeb3a723dfc792665f83411457284b1df
smyaseen <smyaseen164@gmail.com>
2021-10-18 17:30:24 +0500

    pwa

--- src/index.js
+++ src/index.js
@@ -11,9 +11,11 @@ import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import MaterialThemeWrapper from './theme';
 import GlobalStyles from './theme/GlobalStyles';
 import ErrorBoundary from './util/ErrorBoundary/index';
+
 const persistor = persistStore(store);
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -42,6 +44,7 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
